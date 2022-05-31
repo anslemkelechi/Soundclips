e2d17d78090c2e5a9aa8e96f11b311c4ff4195da
Blakcoder <anslemkay180@gmail.com>
2022-05-31 07:21:50 +0100

    Added Mobile Hamburger

---
+++ app.js
@@ -0,0 +1,26 @@
+let mobile = document.querySelector('.mobile-nav')
+let open = document.querySelector('.open')
+let close = document.querySelector('.close')
+let body = document.body
+let mobBack = document.querySelector('.mob-back')
+
+open.addEventListener('click', () => {
+  mobile.classList.toggle('mobile-nav')
+  open.classList.toggle('active')
+  close.classList.toggle('active')
+})
+close.addEventListener('click', () => {
+  mobile.classList.add('mobile-nav')
+  open.classList.toggle('active')
+  close.classList.toggle('active')
+})
+
+window.onscroll = () => {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    mobBack.style.height = '10%'
+    mobBack.style.transition = 'all 0.5s'
+  } else {
+    mobBack.style.height = '0%'
+    mobBack.style.transition = 'all 0.5s'
+  }
+}
